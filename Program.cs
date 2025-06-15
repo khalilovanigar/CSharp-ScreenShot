2682816e91b4c1e32cb7d38fe5cf4119472959ea
khalilovanigar <khalilovanigar64@gmail.com>
2025-06-15 15:47:55 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string imagesPath = Path.Combine(desktop, "Images");
+
+        if (!Directory.Exists(imagesPath))
+            Directory.CreateDirectory(imagesPath);
+
+        System.Console.WriteLine("Click enter to screenshot. Click ESC to exit");
+
+        while (true)
+        {
+            var key = Console.ReadKey(intercept: true);
+            if (key.Key == ConsoleKey.Enter)
+            {
+                string fileName = $"screenshot_{DateTime.Now:yyyyMMdd_HHmmss}.png";
+                string fullPath = Path.Combine(imagesPath, fileName);
+
+                
+                Process.Start("screencapture", $"\"{fullPath}\"");
+
+                System.Console.WriteLine($"ScreenShot saved successfully!: {fileName}");
+            }
+            else if (key.Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+        }
+        
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("Photos on image folder:");
+        string[] files = Directory.GetFiles(imagesPath, "*.png");
+        foreach (string file in files)
+        {
+            System.Console.WriteLine(Path.GetFileName(file));
+        }
+    }
+}
